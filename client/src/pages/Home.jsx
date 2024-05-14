902f6b3583ab6ad6e8b728ee944b056ecc56e3a5
PABLORVLO <pablorevelo209@gmail.com>
2024-05-14 17:23:47 -0500

    proyecto completo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,55 @@
+import { Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
+import { useEffect, useState } from 'react';
+import PostCard from '../components/PostCard';
+
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl font-bold lg:text-6xl text-center'>¡Bienvenido a mi blog!</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>
+        Aquí te sumergirás en una amplia gama de artículos y tutoriales que abordan temas fascinantes como desarrollo web, ingeniería de software y lenguajes de programación. Explora y descubre contenido envolvente que te ayudará a expandir tus conocimientos y habilidades en el apasionante mundo de la tecnología. ¡Prepárate para aprender, inspirarte y crecer como profesional de la programación!
+        </p>
+        <Link
+          to='/search'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+        >
+          Ver todos los artículos
+        </Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction />
+      </div>
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4'>
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
